cdacfce10962fb6c8e5f24531e3d123223f8d053
Federico Mocchetti <fede.mo13990@gmail.com>
2018-04-18 22:07:28 -0300

    Archivo de socket server y saque algunos warning

---
+++ planificador/socketServer.h
@@ -0,0 +1,26 @@
+/*
+ * socketServer.h
+ *
+ *  Created on: Apr 18, 2018
+ *      Author: fmocchetti
+ */
+
+#ifndef SOCKETSERVER_H_
+#define SOCKETSERVER_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/ioctl.h>
+#include <sys/poll.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+#include <netinet/in.h>
+#include <errno.h>
+
+#define SERVER_PORT  12345
+#define TRUE             1
+#define FALSE            0
+
+int createServer(int max_connections, int timeout);
+
+#endif /* SOCKETSERVER_H_ */
