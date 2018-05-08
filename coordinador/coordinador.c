e0265f1d363d9821d9d0688ed94a376238f84cc0
Federico Mocchetti <fede.mo13990@gmail.com>
2018-05-07 22:35:30 -0300

    Rehice el server

--- coordinador/coordinador.c
+++ coordinador/coordinador.c
@@ -1,11 +1,20 @@
-#include "socketServer.h"
+#include "socket.h"
 
 #define IDENTIDAD "coordinador"
+
+int listening_port = 0;
+int cantidad_entradas = 0;
+int size_key = 0;
+int retardo = 0;
+enum {	LSU, EL, KE};
 
 int main (int argc, char *argv[])
 {
 	configure_logger();
+
 	printf("%s\n",IDENTIDAD);
-	create_server(32, 3 * 60 * 1000);
+
+	create_server(32, 20 * 60 * 1000, THREAD_CONNECTION, 12345);
+
 	return 0;
 }
